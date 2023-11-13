619dad3c03a7821d55d5b07db3404d023d572a19
Daniel Tamm <daniel@notima.se>
2023-11-13 17:24:43 +0100

    Add method mapLocationToBusinessPartner

--- src/main/java/org/notima/generic/ifacebusinessobjects/MappingService.java
+++ src/main/java/org/notima/generic/ifacebusinessobjects/MappingService.java
@@ -1,7 +1,12 @@
 package org.notima.generic.ifacebusinessobjects;
 
+import org.notima.generic.businessobjects.BusinessPartner;
+import org.notima.generic.businessobjects.Location;
+
 public interface MappingService {
 
 	public String mapSourceToTarget(String srcValue, String typeOfObject);
+	
+	public BusinessPartner<?> mapLocationToBusinessPartner(Location location, String aptNo);
 	
 }
