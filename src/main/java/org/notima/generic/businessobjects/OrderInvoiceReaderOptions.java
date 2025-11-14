47816012a251961d5b222590511ad3c88834cf12
Daniel Tamm <daniel@notima.se>
2025-11-14 16:59:22 +0100

    Add OrderInvoiceReaderOptions.

---
+++ src/main/java/org/notima/generic/businessobjects/OrderInvoiceReaderOptions.java
@@ -0,0 +1,56 @@
+package org.notima.generic.businessobjects;
+
+import java.time.LocalDate;
+
+/**
+ * Class to specify reader options
+ */
+public class OrderInvoiceReaderOptions {
+
+	private LocalDate	fromDate;
+	private LocalDate	untilDate;
+	private boolean		unpostedOnly;
+	
+	private TaxSubjectIdentifier	bpartner;
+	
+	private boolean		salesOnly;
+	private boolean		vendorOnly;
+	
+	public LocalDate getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(LocalDate fromDate) {
+		this.fromDate = fromDate;
+	}
+	public LocalDate getUntilDate() {
+		return untilDate;
+	}
+	public void setUntilDate(LocalDate untilDate) {
+		this.untilDate = untilDate;
+	}
+	public boolean isUnpostedOnly() {
+		return unpostedOnly;
+	}
+	public void setUnpostedOnly(boolean unpostedOnly) {
+		this.unpostedOnly = unpostedOnly;
+	}
+	public TaxSubjectIdentifier getBpartner() {
+		return bpartner;
+	}
+	public void setBpartner(TaxSubjectIdentifier bpartner) {
+		this.bpartner = bpartner;
+	}
+	public boolean isSalesOnly() {
+		return salesOnly;
+	}
+	public void setSalesOnly(boolean salesOnly) {
+		this.salesOnly = salesOnly;
+	}
+	public boolean isVendorOnly() {
+		return vendorOnly;
+	}
+	public void setVendorOnly(boolean vendorOnly) {
+		this.vendorOnly = vendorOnly;
+	}
+	
+}
